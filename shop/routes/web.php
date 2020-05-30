05e6778e5bef39d643d1c0eb73a8225f8cc1ba55
Nguyễn Văn Tuấn <nvtuan88@gmail.com>
2020-05-31 06:47:05 +0700

    Tuấn Commit

--- shop/routes/web.php
+++ shop/routes/web.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', 'FrontendController@welcom');
+
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Auth::routes();
 

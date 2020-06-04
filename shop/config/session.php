9d5689e4f730409d9ae380927e22d4b7550066d5
Nguyễn Văn Tuấn <nvtuan88@gmail.com>
2020-06-04 20:50:16 +0700

    Tuấn

--- shop/config/session.php
+++ shop/config/session.php
@@ -128,7 +128,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'Writer Shop'), '_').'_session'
     ),
 
     /*
--- shop/resources/views/layouts/frontend.blade.php
+++ shop/resources/views/layouts/frontend.blade.php
@@ -202,13 +202,13 @@
 
                                         <div class="total-cart-price">
                                             <div class="cart-product-line">
-                                                <span>Total</span>
+                                                <span>Tổng</span>
                                                 <span class="total">{{Cart::subtotal()}}</span>
                                             </div>
                                         </div>
                                         <div class="cart-checkout">
                                             <a href="order">
-                                                Check out
+                                                Kiểm tra giỏ
                                                 <i class="fa fa-chevron-right"></i>
                                             </a>
                                         </div>
@@ -229,24 +229,10 @@
 						<div class="mobile-menu">
 							<nav id="dropdown">
 								<ul>
-                                    <li><a href="{{asset('/')}}">HOME</a></li>
+                                    <li><a href="{{asset('/')}}">TRANG CHỦ</a></li>
                                     <li><a href="shop.html">FEATURED</a></li>
                                     <li><a href="shop.html">REVIEW BOOK</a></li>
                                     <li><a href="about.html">ABOUT AUTHOR</a></li>
-                                    <li><a href="#">pages</a>
-                                        <ul>
-                                            <li><a href="about.html">About Us</a></li>
-                                            <li><a href="cart.html">Cart Page</a></li>
-                                            <li><a href="checkout.html">Check Out</a></li>
-                                            <li><a href="contact.html">Contact</a></li>
-                                            <li><a href="{{ route('login') }}">Login</a></li>
-                                            <li><a href="my-account.html">My Account</a></li>
-                                            <li><a href="shop.html">Shopping Page</a></li>
-                                            <li><a href="single-product.html">Single Shop Page</a></li>
-                                            <li><a href="wishlist.html">Wishlist Page</a></li>
-                                            <li><a href="404.html">404 Page</a></li>
-                                        </ul>
-                                    </li>
                                     <li><a href="contact.html">CONTACT</a></li>
 								</ul>
 							</nav>

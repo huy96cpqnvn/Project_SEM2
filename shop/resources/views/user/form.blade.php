a5ef0d848482e07260b9cd52a4b50b46f0a40160
huy96cpqnvn <huy96cpqnvn@gmail.com>
2020-06-06 16:30:10 +0700

    Edit Display
    Sửa giao diện Add, edit user, update icon Add to cart

--- shop/resources/views/user/form.blade.php
+++ shop/resources/views/user/form.blade.php
@@ -1,138 +1,185 @@
 @extends('layouts.backend')
 
 @section('content')
-    @include('template.error')
-    <div class="container white">
-        <div class="row">
-            <div class="container white percent100">
-                <div class="col-lg-12">
-                    <div class="padder-t2">
-                        <h1>Add User</h1>
-                        <div class="horiz-divider"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row padder-t padder-b">
-            <div class="container white">
-                <div class="col-lg-12">
-                    <!-- Form -->
-                    <form class="form-horizontal" action="{{route('user_management.store')}}" method="post" id="contact_form">
-                        @csrf
-                        <input type="hidden" name="_method" value="post">
-                        <fieldset>
-                            <!-- Text input-->
-                            <!-- Text input-->
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-4">
-                                        <label class="control-label pull-right"><h4>User Name</h4></label>
-                                    </div>
-                                    <div class="col-md-4 col-lg-4 inputGroupContainer">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                                            <input id="lastname" name="name" placeholder="User Name" class="form-control" type="text">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Text input-->
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-4">
-                                        <label class="control-label pull-right"><h4>E-Mail</h4></label>
-                                    </div>
-                                    <div class="col-md-4 col-lg-4 inputGroupContainer">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                                            <input id="email" name="email" placeholder="E-Mail Address" class="form-control" type="email" >
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-4">
-                                        <label class="control-label pull-right"><h4>PassWord</h4></label>
-                                    </div>
-                                    <div class="col-md-4 col-lg-4 inputGroupContainer">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                                            <input id="" name="pass" placeholder="PassWord" class="form-control" type="text" >
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-4">
-                                        <label class="control-label pull-right"><h4>level</h4></label>
-                                    </div>
-                                    <div class="col-md-4 col-lg-4 inputGroupContainer">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                                            <select name="level" id="" class="form-control">
-                                                <option value="User">User</option>
-                                            </select>
-{{--                                            <input id="" name="level" placeholder="PassWord" class="form-control" type="text" >--}}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Text input-->
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-4">
-                                        <label class="control-label pull-right"><h4>Phone #</h4></label>
-                                    </div>
-                                    <div class="col-md-4 col-lg-4 inputGroupContainer">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><i class="fa fa-phone"></i></span>
-                                            <input id="phone" name="phone" placeholder="Phone" class="form-control" type="text" >
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-4">
-                                        <label class="control-label pull-right"><h4>Adress </h4></label>
-                                    </div>
-                                    <div class="col-md-4 col-lg-4 inputGroupContainer">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><i class="fa fa-adress"></i></span>
-                                            <input id="address" name="address" placeholder="Adress" class="form-control" type="text" >
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-4">
-                                        <label class="control-label pull-right"><h4>Status </h4></label>
-                                    </div>
-                                    <select class="custom-select" name="status" id="selectBox" onchange="changeFunction();">
-                                        <option value="1" class="btn btn-success" >Active  </option>
-                                        <option value="0" class="btn btn-success" >InActive</option>
-
-
-                                    </select>
-                                </div>
-                            </div>
-                            <!-- Text area -->
-                            <!-- Success message -->
-                            <!-- Button -->
-                            <div class="form-group">
-                                <div class="row">
-                                    <label class="col-md-4 col-lg-4 control-label"></label>
-                                    <div class="col-md-4 col-lg-4">
-                                        <button type="submit" class="btn btn-danger raised">Submit <i class="fa fa-paper-plane"></i></button>
-                                    </div>
-                                </div>
-                            </div>
-                        </fieldset>
-                    </form>
+@include('template.error')
+<div class="container white">
+    <div class="row">
+        <div class="container white percent100">
+            <div class="col-lg-12">
+                <div class="padder-t2">
+                    <h1>Add User</h1>
+                    <div class="horiz-divider"></div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="row padder-t padder-b">
+        <div class="container white">
+            <div class="col-lg-12">
+                <!-- Form -->
+                <form class="form-horizontal" action="{{route('user_management.store')}}" method="post" id="contact_form">
+                    @csrf
+                    <input type="hidden" name="_method" value="post">
+                    <fieldset>
+                        <!-- Text input-->
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-4">
+                                    <label class="control-label pull-right"><h4>User Name</h4></label>
+                                </div>
+                                <div class="col-md-4 col-lg-4 inputGroupContainer">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="mdi mdi-account"></i>
+                                            </span>
+                                        </div>
+                                        <input type="text" id="lastname" name="name" class="form-control" placeholder="Username" aria-label="Username">
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-4">
+                                    <label class="control-label pull-right"><h4>E-Mail</h4></label>
+                                </div>
+                                <div class="col-md-4 col-lg-4 inputGroupContainer">
+
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="mdi mdi-email"></i>
+                                            </span>
+                                        </div>
+                                        <input type="email" id="email" name="email" class="form-control" placeholder="E-Mail Address">
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-4">
+                                    <label class="control-label pull-right"><h4>Password</h4></label>
+                                </div>
+                                <div class="col-md-4 col-lg-4 inputGroupContainer">
+
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="mdi mdi-shield-key"></i>
+                                            </span>
+                                        </div>
+                                        <input type="text" id="pass" name="pass" class="form-control" placeholder="Password">
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div> <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-4">
+                                    <label class="control-label pull-right"><h4>Level</h4></label>
+                                </div>
+                                <div class="col-md-4 col-lg-4 inputGroupContainer">
+
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="mdi mdi-alpha-l-box"></i>
+                                            </span>
+                                        </div>
+
+                                        <select name="level" id="" class="form-control">
+                                            <option value="User">User</option>
+                                        </select>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Text input-->
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-4">
+                                    <label class="control-label pull-right"><h4>Phone </h4></label>
+                                </div>
+                                <div class="col-md-4 col-lg-4 inputGroupContainer">
+
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="mdi mdi-phone"></i>
+                                            </span>
+                                        </div>
+                                        <input type="text" id="phone" name="phone" class="form-control" placeholder="Phone">
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-4">
+                                    <label class="control-label pull-right"><h4>Address </h4></label>
+                                </div>
+                                <div class="col-md-4 col-lg-4 inputGroupContainer">
+
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="mdi mdi-home"></i>
+                                            </span>
+                                        </div>
+                                        <input type="text" id="address" name="address" class="form-control" placeholder="Address">
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-4 col-lg-4">
+                                    <label class="control-label pull-right"><h4>Status </h4></label>
+                                </div>
+
+                                <div class="col-md-4 col-lg-4 inputGroupContainer">
+
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                                <i class="mdi mdi-alpha-s-box"></i>
+                                            </span>
+                                        </div>
+
+                                        <select name="status" id="selectBox" class="form-control" onchange="changeFunction();">
+                                            <option value="1"  >Active  </option>
+                                            <option value="0"  >InActive</option>
+                                        </select>
+
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <!-- Text area -->
+                        <!-- Success message -->
+                        <!-- Button -->
+                        <div class="form-group">
+                            <div class="row">
+                                <label class="col-md-4 col-lg-4 control-label"></label>
+                                <div class="col-md-4 col-lg-4">
+                                    <button type="submit" class="btn btn-danger raised">Submit <i class="fa fa-paper-plane"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
